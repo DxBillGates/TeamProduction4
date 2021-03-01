3b9a555524f0211eccbc753cd402a6ab30345c80
Gates-ToMoPi <67899020+Gates-ToMoPi@users.noreply.github.com>
2021-03-01 14:01:59 +0900

    Add BallBehavior Jump

--- Assets/BallBehavior.cs
+++ Assets/BallBehavior.cs
@@ -10,7 +10,6 @@ public class BallBehavior : MonoBehaviour
     Vector3 groundNormal;
     float speed = 4;
 
-
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,6 +21,10 @@ public class BallBehavior : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
 
         transform.Translate(new Vector3(x, 0, z));
 
@@ -30,31 +33,43 @@ public class BallBehavior : MonoBehaviour
         {
             float distanceOnGround = hit.distance;
             groundNormal = hit.normal;
-            onGround = (distanceOnGround <= 0.5f);
+            if (distanceOnGround <= 0.5f)
+            {
+                onGround = true;
+            }
+            else
+            {
+                onGround = false;
+            }
             Vector3 cross = Vector3.Cross(groundNormal, hit.collider.transform.forward);
             Debug.DrawRay(transform.position, cross);
         }
 
         Vector3 gravity = -groundNormal.normalized;
-        if(!onGround)
+        if (!onGround)
         {
             rb.AddForce(gravity);
+        }
+        else
+        {
+            isJump = false;
         }
 
         Quaternion toRotation = Quaternion.FromToRotation(transform.up, groundNormal) * transform.rotation;
         transform.rotation = toRotation;
 
-        Debug.DrawRay(transform.position, transform.up,new Vector4(0,1,0,1));
+        Debug.DrawRay(transform.position, transform.up, new Vector4(0, 1, 0, 1));
         Debug.DrawRay(transform.position, transform.right, new Vector4(1, 0, 0, 1));
         Debug.DrawRay(transform.position, transform.forward, new Vector4(0, 0, 1, 1));
     }
 
     void Jump()
     {
-        if(!isJump)
+        if (!isJump)
         {
             isJump = true;
-            rb.AddForce(transform.up * 10);
+            rb.AddForce(transform.up.normalized * 100);
+            Debug.Log("jump");
         }
     }
 }
